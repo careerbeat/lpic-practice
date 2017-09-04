74eaa36793503715e6d38e6538b6460788f91b67
daidai02 <daidai2604mark2@gmail.com>
2017-09-04 16:57:44 +0900

    コマンド1問プログラムサンプル

---
+++ regist.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+if ($_POST['command'] === "pwd")
+{
+header("Location: http://192.168.24.1/rei.php");
+}
+else
+{
+header("Location: http://192.168.24.1/form.html");
+}
+?>
